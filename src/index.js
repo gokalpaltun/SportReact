85710f3c67b5b4d110a0f9e9428e17e17119b640
gokalpaltun <gokalpaltun1994@gmail.com>
2019-07-28 16:39:37 +0300

    Add helper chrome extension for state management

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import allReducers from "./reducers/index";
 
-const store = createStore(allReducers);
+const store = createStore(
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
   <Provider store={store}>
